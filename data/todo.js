58946fa5fae1a2fc6ede561e38d5ba3ca1d249e5
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-11-10 16:21:29 -0800

    routes added

---
+++ data/todo.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    id: 1,
+    todo: 'fold laundry',
+    completed: false,
+  },
+  {
+    id: 2,
+    todo: 'do dishes',
+    completed: false,
+  },
+  {
+    id: 3,
+    todo: 'wipe down shower walls',
+    completed: false,
+  },
+  {
+    id: 4,
+    todo: 'replace lightbulb'
+  }
+];
